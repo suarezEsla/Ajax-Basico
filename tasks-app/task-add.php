778993e09f3189407ceb737934374c9c00e22c29
Esla <suarezesla@gmail.com>
2022-05-23 13:41:47 +0200

    Tasks
    Create, list, delete with JSON, AJAX, PHP, JS

---
+++ tasks-app/task-add.php
@@ -0,0 +1,17 @@
+<?php
+
+include('database.php');
+
+if(isset($_POST['name'])){
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+
+    $query = "INSERT INTO task (name,description) VALUES ('$name','$description')";
+    $result = mysqli_query($connection,$query);
+    if(!$result){
+        die('Query failed');
+    }
+    echo 'Task added successfully';
+}
+
+?>
